ae391e0775edcca364c520f00eeed83759026a14
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-06-13 01:51:05 +0300

    Update البنك.py

--- jepthon/plugins/البنك.py
+++ jepthon/plugins/البنك.py
@@ -25,12 +25,12 @@ async def amireallyalive(event):
     "للتـأكد من ان البـوت يعـمـل"
     reply_to_id = await reply_id(event)
     start = datetime.now()
-    await edit_or_reply(event, "** ⌯︙يتـم التـأكـد انتـظر قليلا رجاءا**")
+    await edit_or_reply(event, "** ⌯︙يتـم التـأكـد من البنك انتـظر قليلا رجاءا**")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     EMOJI = gvarstatus("ALIVE_EMOJI") or "✇ ◅"
     PING_TEXT = gvarstatus("PING_TEXT") or "**[ 𝗜 𝗝𝘂𝘀𝘁 𝗔𝘀𝗸𝗲𝗱 𝗙𝗼𝗿 𝗦𝗼𝗺𝗲 𝗣𝗲𝗮𝗰𝗲 🎀 ](t.me/Jepthon)**"
-    PING_IMG = gvarstatus("PING_PIC") or Config.P_PIC
+    PING_IMG = gvarstatus("PING_PIC") or Config.P_PIC or "https://telegra.ph/file/b7aebda65e3df4906f5dc.jpg"
     jepthon_caption = gvarstatus("PING_TEMPLATE") or temp
     caption = jepthon_caption.format(
         PING_TEXT=PING_TEXT,
