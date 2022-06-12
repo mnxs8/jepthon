3d0b37b1b11e428d828f0f1a6443f0fed004d074
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-06-13 01:24:02 +0300

    Update البنك.py

--- jepthon/plugins/البنك.py
+++ jepthon/plugins/البنك.py
@@ -36,7 +36,7 @@ async def _(event):
         EMOJI=EMOJI,
         ping=ms,
     )
-if PING_PIC:
+    if PING_PIC:
         RR7 = [x for x in PING_PIC.split()]
         PIC = random.choice(RR7)
         try:
@@ -47,13 +47,13 @@ if PING_PIC:
         except (WebpageMediaEmptyError, MediaEmptyError, WebpageCurlFailedError):
             return await edit_or_reply(
                 event,
-                f"**الميـديا خـطأ **\nغـير الرابـط بأستـخدام الأمـر  \n `.اضف_فار PING_PIC رابط صورتك`\n\n**لا يمـكن الحـصول عـلى صـورة من الـرابـط :-** `{PIC}`",
+                f"**الميـديا خـطأ **\nغـير الرابـط بأستـخدام الأمـر  \n `.اضف_فار ALIVE_PIC رابط صورتك`\n\n**لا يمـكن الحـصول عـلى صـورة من الـرابـط :-** `{PIC}`",
             )
-else:
-     await edit_or_reply(
-         event,
-         caption,
-     )
+    else:
+        await edit_or_reply(
+            event,
+            caption,
+        )
 
 temp = """{PING_TEXT}
 **{EMOJI} البنك ↜ :** `{ping}`"""
