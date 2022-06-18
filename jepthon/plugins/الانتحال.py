67a7416413ec1a98284321a7e26c7b4fe566cbde
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-06-18 23:43:11 +0300

    Update الانتحال.py

--- jepthon/plugins/الانتحال.py
+++ jepthon/plugins/الانتحال.py
@@ -38,7 +38,7 @@ async def _(event):
     "To clone account of mentiond user or replied user"
     replied_user, error_i_a = await get_user_from_event(event)
     if replied_user.id == 705475246 or 393120911 or 1374312239:
-        return await edit_delete(event, "لا تحاول تنتحل المطورين لك!")
+        return await edit_delete(event, "**لا تحاول تنتحل المطورين ادبسز!**")
     if replied_user is None:
         return
     user_id = replied_user.id
