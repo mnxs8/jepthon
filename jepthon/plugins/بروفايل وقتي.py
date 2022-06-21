c949bcdd3a9edc38a2d8c85387fe2c3be5c98516
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-06-21 04:53:09 +0300

    Update بروفايل وقتي.py

--- jepthon/plugins/بروفايل وقتي.py
+++ jepthon/plugins/بروفايل وقتي.py
@@ -61,7 +61,7 @@ async def digitalpicloop():
         img = Image.open(autophoto_path)
         drawn_text = ImageDraw.Draw(img)
         fnt = ImageFont.truetype(jep, 65)
-        drawn_text.text((200, 200), current_time, font=fnt, fill=webcolors.name_to_rgb(colorco) or (250, 0, 0))
+        drawn_text.text((200, 200), current_time, font=fnt, fill=webcolors.name_to_rgb(colorco) or (250, 250, 250))
         img.save(autophoto_path)
         file = await jmthon.upload_file(autophoto_path)
         try:
