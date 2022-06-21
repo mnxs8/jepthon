7fecf0b5027483557bb3a064c096d3e0ebf7f95b
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-06-21 09:48:11 +0300

    Update اوامر التخزين.py

--- jepthon/plugins/اوامر التخزين.py
+++ jepthon/plugins/اوامر التخزين.py
@@ -118,11 +118,11 @@ async def log(log_text):
     if BOTLOG:
         if log_text.reply_to_msg_id:
             reply_msg = await log_text.get_reply_message()
-            await reply_msg.forward_to(BOTLOG_CHATID)
+            await reply_msg.forward_to(Config.PM_LOGGER_GROUP_ID)
         elif log_text.pattern_match.group(1):
             user = f"#التخــزين / ايـدي الدردشــه : {log_text.chat_id}\n\n"
             textx = user + log_text.pattern_match.group(1)
-            await log_text.client.send_message(BOTLOG_CHATID, textx)
+            await log_text.client.send_message(Config.PM_LOGGER_GROUP_ID, textx)
         else:
             await log_text.edit("**⌔┊بالــرد على اي رسـاله لحفظهـا في كـروب التخــزين**")
             return
