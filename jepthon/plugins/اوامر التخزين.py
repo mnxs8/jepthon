80d62c38f80fb4136e2b9bd51c5c312d7dbdf0b6
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-06-21 09:15:12 +0300

    Update اوامر التخزين.py

--- jepthon/plugins/اوامر التخزين.py
+++ jepthon/plugins/اوامر التخزين.py
@@ -64,6 +64,115 @@ async def monito_p_m_s(event):
                 LOG_CHATS_.COUNT += 1
             except Exception as e:
                 LOGS.warn(str(e))
+@jmthon.ar_cmd(incoming=True, func=lambda e: e.mentioned, edited=False, forword=None)
+async def log_tagged_messages(event):
+    hmm = await event.get_chat()
+    from .afk import AFK_
+
+    if gvarstatus("GRPLOG") and gvarstatus("GRPLOG") == "false":
+        return
+    if (
+        (no_log_pms_sql.is_approved(hmm.id))
+        or (Config.PM_LOGGER_GROUP_ID == -100)
+        or ("on" in AFK_.USERAFK_ON)
+        or (await event.get_sender() and (await event.get_sender()).bot)
+    ):
+        return
+    full = None
+    try:
+        full = await event.client.get_entity(event.message.from_id)
+    except Exception as e:
+        LOGS.info(str(e))
+    messaget = media_type(event)
+    resalt = f"#التــاكــات\n\n<b>⌔┊الكــروب : </b><code>{hmm.title}</code>"
+    if full is not None:
+        resalt += (
+            f"\n\n<b>⌔┊المـرسـل : </b> {_format.htmlmentionuser(full.first_name , full.id)}"
+        )
+    if messaget is not None:
+        resalt += f"\n\n<b>⌔┊رسـالـة ميـديـا : </b><code>{messaget}</code>"
+    else:
+        resalt += f"\n\n<b>⌔┊الرســالـه : </b>{event.message.message}"
+    resalt += f"\n\n<b>⌔┊رابـط الرسـاله : </b><a href = 'https://t.me/c/{hmm.id}/{event.message.id}'> link</a>"
+    if not event.is_private:
+        await event.client.send_message(
+            Config.PM_LOGGER_GROUP_ID,
+            resalt,
+            parse_mode="html",
+            link_preview=False,
+        )
+
+
+@jmthon.ar_cmd(
+    pattern="خزن(?:\s|$)([\s\S]*)",
+    command=("خزن", plugin_category),
+    info={
+        "header": "To log the replied message to bot log group so you can check later.",
+        "الاسـتخـدام": [
+            "{tr}خزن",
+        ],
+    },
+)
+async def log(log_text):
+    "To log the replied message to bot log group"
+    if BOTLOG:
+        if log_text.reply_to_msg_id:
+            reply_msg = await log_text.get_reply_message()
+            await reply_msg.forward_to(BOTLOG_CHATID)
+        elif log_text.pattern_match.group(1):
+            user = f"#التخــزين / ايـدي الدردشــه : {log_text.chat_id}\n\n"
+            textx = user + log_text.pattern_match.group(1)
+            await log_text.client.send_message(BOTLOG_CHATID, textx)
+        else:
+            await log_text.edit("**⌔┊بالــرد على اي رسـاله لحفظهـا في كـروب التخــزين**")
+            return
+        await log_text.edit("**⌔┊تـم الحفـظ في كـروب التخـزين .. بنجـاح ✓**")
+    else:
+        await log_text.edit("**⌔┊عـذراً .. هـذا الامـر يتطلـب تفعيـل فـار التخـزين اولاً**")
+    await asyncio.sleep(2)
+    await log_text.delete()
+
+
+@jmthon.ar_cmd(
+    pattern="تفعيل التخزين$",
+    command=("تفعيل التخزين", plugin_category),
+    info={
+        "header": "To turn on logging of messages from that chat.",
+        "الاسـتخـدام": [
+            "{tr}log",
+        ],
+    },
+)
+async def set_no_log_p_m(event):
+    "To turn on logging of messages from that chat."
+    if Config.PM_LOGGER_GROUP_ID != -100:
+        chat = await event.get_chat()
+        if no_log_pms_sql.is_approved(chat.id):
+            no_log_pms_sql.disapprove(chat.id)
+            await edit_delete(
+                event, "**⌔┊تـم تفعيـل التخـزين لهـذه الدردشـه .. بنجـاح ✓**", 5
+            )
+
+
+@jmthon.ar_cmd(
+    pattern="تعطيل التخزين$",
+    command=("تعطيل التخزين", plugin_category),
+    info={
+        "header": "To turn off logging of messages from that chat.",
+        "الاسـتخـدام": [
+            "{tr}nolog",
+        ],
+    },
+)
+async def set_no_log_p_m(event):
+    "To turn off logging of messages from that chat."
+    if Config.PM_LOGGER_GROUP_ID != -100:
+        chat = await event.get_chat()
+        if not no_log_pms_sql.is_approved(chat.id):
+            no_log_pms_sql.approve(chat.id)
+            await edit_delete(
+                event, "**⌔┊تـم تعطيـل التخـزين لهـذه الدردشـه .. بنجـاح ✓**", 5
+            )
                 
 @jmthon.ar_cmd(
     pattern="تخزين الخاص (تشغيل|ايقاف)$",
