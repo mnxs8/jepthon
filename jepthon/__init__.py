cff6ef366d1ad228324452448be95b033ba1a574
المطور مصطفى /dev Mustfa <74466394+mnxs8@users.noreply.github.com>
2022-07-11 21:52:09 +0300

    Update __init__.py

--- jepthon/__init__.py
+++ jepthon/__init__.py
@@ -1,3 +1,27 @@
+import time
+
+import heroku3
+
+from .Config import Config
+from .core.logger import logging
+from .core.session import jepiq
+from .sql_helper.globals import addgvar, delgvar, gvarstatus
+
+__version__ = "1.0.0"
+__license__ = "كـتابة وتـعديل فريـق جيبثون"
+__author__ = "جيبثون <https://T.ME/Jepthon>"
+__copyright__ = "JEPTHON TEAM (C) 2020 - 2021  " + __author__
+
+jepiq.version = __version__
+jepiq.tgbot.version = __version__
+LOGS = logging.getLogger("jepthon")
+bot = jepiq
+
+StartTime = time.time()
+JMVERSION = "5.2.0"
+
+
+if Config.UPSTREAM_REPO == "jepthoniq":
     UPSTREAM_REPO_URL = "https://github.com/jepthoniq/jepthon"
 else:
     UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
