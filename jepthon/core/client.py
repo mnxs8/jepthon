c0571761e6f54e43cb6c13acfbf10b43433b39de
المطور مصطفى /dev Mustfa <74466394+mnxs8@users.noreply.github.com>
2022-07-11 03:06:56 +0300

    Update client.py

--- jepthon/core/client.py
+++ jepthon/core/client.py
@@ -140,8 +140,8 @@ class CatUserBotClient(TelegramClient):
                         pastelink = await paste_message(
                             ftext, pastetype="s", markdown=False
                         )
-                        text = "**تقرير خطا جيبثون**\n\n"
-                        link = "[هنا](https://t.me/JepthonSupport)"
+                        text = "**تقرير خطا لندا**\n\n"
+                        link = "[هنا](https://t.me/egbbb)"
                         text += "إذا كنت تريد يمكنك الإبلاغ عن ذلك"
                         text += f"- فقط قم بإعادة توجيه هذه الرسالة {link}.\n"
                         text += "لا يتم تسجيل اي خطا فقط التاريخ والوقت\n\n"
@@ -255,8 +255,8 @@ class CatUserBotClient(TelegramClient):
                         pastelink = await paste_message(
                             ftext, pastetype="s", markdown=False
                         )
-                        text = "**تقرير خطا جيبثون**\n\n"
-                        link = "[هنا](https://t.me/GroupJepthon)"
+                        text = "**تقرير خطا لندا**\n\n"
+                        link = "[هنا](https://t.me/egbbb)"
                         text += "إذا كنت تريد يمكنك الإبلاغ عن ذلك"
                         text += f"- فقط قم بإعادة توجيه هذه الرسالة {link}.\n"
                         text += "لا يتم تسجيل اي خطا فقط التاريخ والوقت\n\n"
